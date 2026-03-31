db737b28a33b82a644f38ee3f22dc7714d029181
lilyaleong-netizen <lilyaleong@gmail.com>
2026-03-31 15:07:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  define: {
+    // 全局站点域名常量
+    SITE_DOMAIN: JSON.stringify('https://yeld-tech.com')
   }
 })
+
